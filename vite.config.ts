87aa4d7e87413f37da3bc7997cbd0baec094e906
changweihua <changweihua@outlook.com>
2023-06-25 11:16:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import AutoImport from 'unplugin-auto-import/vite'
-import IconsResolver from 'unplugin-icons/resolver'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
-import Icons from 'unplugin-icons/vite'
+import AutoImport from "unplugin-auto-import/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+import Icons from "unplugin-icons/vite";
 
 function pathResolve(dir: string) {
   return resolve(__dirname, ".", dir);
@@ -12,40 +11,40 @@ function pathResolve(dir: string) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins:[AutoImport({
-    resolvers: [
-      IconsResolver({
-        prefix: 'i'
-      })
-    ]
-  }),
-  Components({
-    resolvers: [
-      IconsResolver({
-        enabledCollections: [
-          'ep',
-          'ant-design',
-          'icon-park',
-          'openmoji',
-          'material-symbols',
-          'svg-spinners',
-          'fluent',
-          'wi',
-          'line-md',
-          'ic',
-          'svg-spinners',
-          'prime',
-          'icon-park-outline',
-          'arcticons',
-          'mdi',
-          'carbon'
-        ]
-      })
-    ]
-  }),
-  Icons({ autoInstall: true }),
-],
-
+  plugins: [
+    AutoImport({
+      resolvers: [
+        IconsResolver({
+          prefix: "i",
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        IconsResolver({
+          enabledCollections: [
+            "ep",
+            "ant-design",
+            "icon-park",
+            "openmoji",
+            "material-symbols",
+            "svg-spinners",
+            "fluent",
+            "wi",
+            "line-md",
+            "ic",
+            "svg-spinners",
+            "prime",
+            "icon-park-outline",
+            "arcticons",
+            "mdi",
+            "carbon",
+          ],
+        }),
+      ],
+    }),
+    Icons({ autoInstall: true }),
+  ],
   ssr: {
     noExternal: ["vitepress-plugin-nprogress"],
   },
