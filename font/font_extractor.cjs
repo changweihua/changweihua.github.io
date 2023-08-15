5cd5661af6d5d05d4a2823ee4984f5fe4bd521ef
changweihua <changweihua@outlook.com>
2023-08-15 10:21:31 +0800

    chore(custom): 增加font-spider

---
+++ font/font_extractor.cjs
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+console.log('------------------start--------------------')
+let filesList = []; //vue 文件路径列表
+let jsFilesList = []; //ts 文件路径列表
+function readFileList(dir, filesList = []) {
+console.log(`------------------${dir}--------------------`)
+  const files = fs.readdirSync(dir);
+  files.forEach((item) => {
+    var fullPath = path.join(dir, item);
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory()) {
+      readFileList(path.join(dir, item), filesList); //递归读取文件
+    } else if (fullPath.endsWith(".vue")) {
+      filesList.push(fullPath);
+    } else if (fullPath.endsWith(".md")) {
+      filesList.push(fullPath);
+    } else if (/(.ts|.js)$/g.test(fullPath)) {
+      jsFilesList.push(fullPath);
+    }
+  });
+  return filesList;
+}
+
+readFileList(path.resolve(__dirname, "../blog"), filesList);
+readFileList(path.resolve(__dirname, "../category"), filesList);
+readFileList(path.resolve(__dirname, "../gallery"), filesList);
+
+fs.stat(path.resolve(__dirname, "src/local"), function (err, statObj) {
+  // 判断local文件是否存在，如果不存在则创建，如果创建则直接处理json文件
+  if (!statObj) {
+    fs.mkdir(path.resolve(__dirname, "src/local"), function (err) {
+      writeFile(filesList, "index");
+      writeFile(jsFilesList, "jsIndex");
+    });
+  } else {
+    writeFile(filesList, "index");
+    writeFile(jsFilesList, "jsIndex");
+  }
+});
+function writeFile(fileArr, fileName) {
+  const reg_1 =
+    /(?<!\/\/\s*.*|<!--\s.*)([\u2E80-\u9FFF]*\$?{{0,2}\w*\.*\w*}{0,2}[\u2E80-\u9FFF]+)*/g;
+  const obj = fileArr.reduce((pre, cur) => {
+    let fileSuffix = cur.match(/(.ts|.js|.vue|.md)$/g)?.[0];
+    let pathName = path.basename(cur, fileSuffix);
+    // 如果文件名是index,取父级文件名
+    if (pathName === "index") {
+      const pathArr = cur.split(path.sep);
+      pathName = pathArr[pathArr.length - 2];
+    }
+    pre[pathName] = {};
+    let pkg = fs.readFileSync(cur, "utf-8");
+    const strArr = pkg.match(reg_1);
+    if (strArr?.length) {
+      strArr.forEach((item, index) => {
+        if (item.includes("{")) {
+          let index = 0;
+          item = item.replace(/\$?{{0,2}\w*\.*\w*}{0,2}/g, function (val) {
+            if (val) {
+              index++;
+              return `{${index - 1}}`;
+            } else {
+              return "";
+            }
+          });
+        }
+        // 如果匹配的字符串的字数大于10，处理key 值
+        if (item.length) {
+          let str = item.length >= 10 ? `${item.substring(0, 7)}...` : item;
+          pre[pathName][str] = item;
+        }
+      });
+    }
+    return pre;
+  }, {});
+  // 创建json文件
+  fs.writeFile(
+    path.resolve(__dirname, `src/local/${fileName}.json`),
+    JSON.stringify(obj),
+    "utf8",
+    function (err) {
+      // console.log(err)
+    }
+  );
+}
---
+++ font/get_char.cjs
@@ -0,0 +1,75 @@
+const fs = require("fs");
+
+/*
+fs.stat               检测是文件还是目录
+fs.mkdir              创建目录
+fs.writeFile          创建写入文件
+fs.appendFile         追加文件
+fs.readFile           读取文件
+fs.readdir            读取目录
+fs.rename             重命名  1,表示重命名 2,移动文件
+fs.unlink             删除文件
+fs.rmdir              删除目录
+*/
+
+// 相对路径 目录地址
+const path = "../src/local/";
+
+// 是否为目录
+function isDir(path) {
+  return new Promise((resolve, rejects) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (stats.isDirectory()) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+}
+
+// 读取目录文件
+const redPath = (path) => {
+  fs.readdir(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    data.forEach(async (item) => {
+      let pathItem = path + item;
+      if (!(await isDir(pathItem))) {
+        ocrText(pathItem);
+      } else redPath(pathItem + "/");
+    });
+  });
+};
+
+// 识别文字
+const ocrText = (pathItem) => {
+  const reg = /['"][\u4e00-\u9fa5]+.*['"]/g;
+  const res = fs.readFileSync(pathItem, "utf8");
+  const lines = res.split(/\r?\n/);
+  append("\n 路径：" + pathItem);
+  lines.forEach((item) => {
+    if (item.match(reg)) {
+      append(item.match(reg)[0]);
+    }
+  });
+};
+
+// 读入文件内容
+const append = (text) => {
+  fs.appendFile("./translateFiles.txt", "\n" + text, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("写入成功");
+  });
+};
+
+redPath(path);
