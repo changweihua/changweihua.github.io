94b6bd4f4c439495a094e8cf6211a4a966febcf3
changweihua <changweihua@outlook.com>
2023-08-17 14:55:29 +0800

    refactor(custom): 优化gsap引用

--- .vitepress/components/AnimationTitle.vue
+++ .vitepress/components/AnimationTitle.vue
@@ -4,16 +4,16 @@ import { onMounted } from "vue";
 import gsap from "gsap";
 
 // get other plugins:
-import ScrollTrigger from "gsap/ScrollTrigger";
-import Flip from "gsap/Flip";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// import Flip from "gsap/Flip";
 // import Draggable from "gsap/Draggable";
 
 // or all tools are exported from the "all" file (excluding members-only plugins):
 // import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
-import { Draggable, MotionPathPlugin } from "gsap/all";
+// import { Draggable, MotionPathPlugin } from "gsap/all";
 
 // don't forget to register plugins
-gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin);
+// gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin);
 
 const props = defineProps({
   name: {
