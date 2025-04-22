757b99f51a519dae33abb4128afa502260a584c0
changweihua <changweihua@outlook.com>
2025-04-23 06:22:41 +0800

    :bug: fix: Update head.ts

--- .vitepress/src/head.ts
+++ .vitepress/src/head.ts
@@ -27,7 +27,7 @@ export const head: HeadConfig[] = [
   //     // })()
   //     // </script>
   //   ]
-  // ["meta", { name: "theme-color", content: "#ffffff" }],
+  ["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0, viewport-fit=cover" }],
   ["meta", { name: "mobile-web-app-capable", content: "yes" }],
   ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
   [
