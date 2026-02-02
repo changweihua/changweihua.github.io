905c41ca03d6975c95802de44d2dc35c43b9cefb
changweihua <changweihua@outlook.com>
2026-02-02 20:51:38 +0800

    :bug: fix: Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,12 @@
 import { postcssIsolateStyles } from 'vitepress'
-import UnoCSS from 'unocss/postcss'
+// import UnoCSS from 'unocss/postcss'
 
 export default {
   plugins: [
-    UnoCSS(), // 作为 PostCSS 插件运行
+    // UnoCSS(), // 作为 PostCSS 插件运行
     postcssIsolateStyles({
       //includeFiles: [/vp-doc\.css/, /base\.css/],
     }),
   ],
 }
+
